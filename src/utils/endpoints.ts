f6fe00c738068f0b5e275b23eb5b2a79a7824d90
tb0se <rachiditbose@gmail.com>
2024-01-06 18:55:42 +0200

    update base url

--- src/utils/endpoints.ts
+++ src/utils/endpoints.ts
@@ -1,12 +1,12 @@
-const baseUrl = import.meta.env.VITE_BASE_URL;
+const baseUrl = import.meta.env.VITE_BE_BASE_URL;
 
 export const loginUrl = `${baseUrl}/login`;
 export const registerUrl = `${baseUrl}/register`;
 export const checkAuthPath = `${baseUrl}/check-auth`;
 export const profilePath = `${baseUrl}/profile`;
 
-export const socketUrl = "ws://localhost:5000/api/chat/ws";
+export const socketUrl = 'ws://localhost:5000/api/chat/ws';
 export const contactsUrl = `${baseUrl}/contact`;
 export const messagesUrl = `${baseUrl}/messages`;
-export const avatarUrl = "https://api.multiavatar.com/";
+export const avatarUrl = 'https://api.multiavatar.com/';
 export const usersUrl = `${baseUrl}/users/`;
